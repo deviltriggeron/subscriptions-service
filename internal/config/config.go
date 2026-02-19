c402993c28ec3ea0a4c982e50b2f6a6304a59694
Starfighter Dollie <starfigd@student.21-school.ru>
2026-02-19 23:03:59 +0900

    chore: add domain, dto, db setup and env config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,37 @@
 package config
+
+import (
+	"log"
+	"os"
+	"subscriptions-service/internal/domain"
+	"sync"
+
+	"github.com/joho/godotenv"
+)
+
+var once sync.Once
+
+func loadEnv() {
+	once.Do(func() {
+		if err := godotenv.Load("config.env"); err != nil {
+			log.Println("config.env not found, using system env")
+		}
+	})
+}
+
+func GetDBConfig() domain.DBConfig {
+	loadEnv()
+
+	return domain.DBConfig{
+		User: os.Getenv("POSTGRES_USER"),
+		Pass: os.Getenv("POSTGRES_PASSWORD"),
+		DB:   os.Getenv("POSTGRES_DB"),
+		Host: os.Getenv("POSTGRES_HOST"),
+		Port: os.Getenv("POSTGRES_PORT"),
+	}
+}
+
+func GetServerPort() string {
+	loadEnv()
+	return os.Getenv("SERVER_PORT")
+}
---
+++ internal/domain/config.go
@@ -0,0 +1,9 @@
+package domain
+
+type DBConfig struct {
+	User string
+	Pass string
+	DB   string
+	Host string
+	Port string
+}
